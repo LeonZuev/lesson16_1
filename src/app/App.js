bd520481d8a09c2d0028817e0b15e0930307a20e
Leon Zuev <loonatune@gmail.com>
2023-05-26 21:07:46 +0300

    Add count

--- src/app/App.js
+++ src/app/App.js
@@ -1,12 +1,29 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  // let count = 0
+  // count - переменная
+  // setCount - функция, которая меняет переменную count
+  const [count, setCount] = useState(0)
+
+  function handleClickMinus() {
+    setCount(count - 1) 
+  }
+  function handleClickPlus() {
+    setCount(count + 1) 
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-      <p></p>
-
+        <p>Hello</p>
+        <div>
+        <button onClick={handleClickMinus}>-</button> {"  "}
+        <span>{count}</span> {"  "}
+        <button onClick={handleClickPlus}>+</button>
+        </div>
+        <input value={0}/>
       </header>
     </div>
   );
